5335b7bbcb881021a10bc8d08f8b3994fc50e054
2192375d <173829746yzpq@gmail.com>
2025-10-24 11:24:40 -0400

    feat: made random item generating, item collection and refined inventory system and inventory display

---
+++ scripts/overworld/display/inventory-slot-ui.gd
@@ -0,0 +1,14 @@
+extends Panel
+
+@export var item: Item
+@export var inventory: Inventory = load("res://inventory/Inventory.tres")
+@export var item_display: Sprite2D
+@export var label: Label
+
+func _ready() -> void:
+	item_display.texture = item.texture
+	label.text = str(inventory.items[item])
+	GlobalSignal.inventory_update.connect(_on_inventory_update)
+
+func _on_inventory_update() -> void:
+	label.text = str(inventory.items[item])
