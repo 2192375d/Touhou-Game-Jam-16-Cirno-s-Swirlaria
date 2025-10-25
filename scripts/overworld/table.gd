f3cd7c7b64366575d61b0fa52fd0c9c38170cece
yoshixi <davidchenyuhe@proton.me>
2025-10-24 22:11:25 -0400

    temp: temporary text show up and hover over overworld table, bad code but its ok

--- scripts/overworld/table.gd
+++ scripts/overworld/table.gd
@@ -3,18 +3,22 @@ extends StaticBody2D
 @onready var area : Area2D = get_node("Area2D")
 @onready var hoversprite : Sprite2D = get_node("HoverSprite")
 
+var general_text : RichTextLabel
+
 func _ready() -> void:
 	area.body_entered.connect(_on_body_entered)
 	area.body_exited.connect(_on_body_exited)
+	general_text = 	GlobalText.create_text("Space to Interact", Vector2(5,20))
+	add_child(general_text)
 
 func _on_body_entered(body: Node):
 	if body is Player:
 		hoversprite.visible = true
-		GlobalSignal.display_tooltip.emit("Space to Interact", true)
 		print("player entered")
+		general_text.visible = true
 
 func _on_body_exited(body: Node):
 	if body is Player:
 		hoversprite.visible = false
-		GlobalSignal.display_tooltip.emit("", false)
 		print("player exited")
+		general_text.visible = false
