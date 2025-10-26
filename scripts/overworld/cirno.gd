aafe71ca856fa15af47eba6ebc7a579611e78a5a
digitalyoshixi <digitalyoshixi@proton.me>
2025-10-25 22:33:10 -0400

    fix: fixing health and invincibility

--- scripts/overworld/cirno.gd
+++ scripts/overworld/cirno.gd
@@ -2,12 +2,11 @@ extends CharacterBody2D
 
 class_name Player
 
+@onready var invincible_timer : Timer = get_node("InvincibilityTimer")
+
 @export var movement_component: MovementComponent
 @export var animation_component: AnimationComponent
-
 @export var inventory: Inventory
-@export var HP: int = 3
-
 @export var spawn_point: Vector2 = Vector2.ZERO
 
 func _ready() -> void:
@@ -19,13 +18,16 @@ func _process(_delta: float) -> void:
 		Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left"),
 		Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
 	)
-	
 	movement_component.handle_movement(move_indent)
 	animation_component.handle_animation(move_indent)
 	move_and_slide()
-
+	
 func _on_hitbox_area_entered(area: Area2D) -> void:
-	if area is Bullet:
-		HP -= 1
+	print("hello")
+	print(invincible_timer.is_stopped())
+	if area is Bullet and invincible_timer.is_stopped():
+		GlobalState.hp -= 1
+		GlobalSignal.hp_update.emit()
 		position = spawn_point
-		print("you lost a HP! HP = ", HP)
+		print("you lost a HP! HP = ", GlobalState.hp)
+		invincible_timer.start(1) # start for one second
--- scripts/singletons/global-signal.gd
+++ scripts/singletons/global-signal.gd
@@ -3,3 +3,4 @@ extends Node
 signal inventory_update()
 signal pattern_end()
 signal score_update()  # it gets its value from global-state
+signal hp_update()  # it gets its value from global-state
