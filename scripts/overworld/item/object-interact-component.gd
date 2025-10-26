fcb44d3b17754ca8e2508217f8f32e470e324df2
digitalyoshixi <digitalyoshixi@proton.me>
2025-10-25 23:09:24 -0400

    feat: new hover effect over interactables

--- scripts/overworld/item/object-interact-component.gd
+++ scripts/overworld/item/object-interact-component.gd
@@ -3,6 +3,7 @@ extends Node
 class_name ObjectInteractComponent
 
 @export var area: Area2D
+@export var sprite : Sprite2D
 @onready var inArea: bool = false
 
 signal interacted()
@@ -16,12 +17,16 @@ func _on_body_entered(body: Node):
 	# GlobalSignal.score_update.emit()
 	if body is Player:
 		inArea = true
-		#print("player entered")
+		print("player entered")
+		if sprite:
+			sprite.modulate = Color(10, 10, 10, 1)
 
 func _on_body_exited(body: Node):
 	if body is Player:
 		inArea = false
-		#print("player exited")
+		print("player exited")
+		if sprite:
+			sprite.modulate = Color(1, 1, 1, 1)
 
 func _process(_delta: float) -> void:
 	if Input.is_action_just_pressed("ui_accept") && inArea == true:
