f2ab943eac65f216a0984d307f80eeb9d39d9e9c
digitalyoshixi <digitalyoshixi@proton.me>
2025-10-27 02:25:59 -0400

    feat: game over win

--- scripts/overworld/game_overworld.gd
+++ scripts/overworld/game_overworld.gd
@@ -8,7 +8,6 @@ func _ready() -> void:
 	GlobalSignal.hp_update.connect(_on_hp_update)
 	time_elapsed_timer.start(GlobalState.time)
 
-
 func _on_bulletzone_area_exited(area: Area2D) -> void:
 	if area is Bullet:
 		area.queue_free()
@@ -20,6 +19,11 @@ func _on_hp_update() -> void:
 func _process(_delta: float) -> void:
 	time_label.text = "time left: " + str(int(time_elapsed_timer.time_left))
 
-
 func _on_timeelapsedtimer_timeout() -> void:
-	get_tree().change_scene_to_file("res://scenes/game_endpage.tscn")
+	if (GlobalState.score >= 50000):
+		get_tree().change_scene_to_file("res://scenes/game_endpage.tscn")
+	else:
+		get_tree().change_scene_to_file("res://scenes/game_endpage.tscn")
+		pass
+		# game over scene
+		# get_tree().change_scene_to_file("res://scenes/game_endpage.tscn")
