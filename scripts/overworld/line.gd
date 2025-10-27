77f1fa85673ec0f8cc22ce1dd8576af4104dabb1
2192375d <173829746yzpq@gmail.com>
2025-10-27 14:17:35 -0400

    fix: added more hints for player

--- scripts/overworld/line.gd
+++ scripts/overworld/line.gd
@@ -2,12 +2,17 @@ extends Node2D
 
 @onready var visitor : MarginContainer = get_node("Visitor")
 @onready var vbox : VBoxContainer = get_node("Control/VBoxContainer")
-
+@export var order_popout: Panel
+@export var popout_timer: Timer
 
 static var allvisitors : Array[MarginContainer]
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	order_popout.hide() # by lumiere
+	popout_timer.autostart = false # by lumiere
+	popout_timer.one_shot = true # by lumiere
+	
 	print("HELLO I AM STARTED")
 	# hydrate the list of orders first
 	for a in GlobalState.orders.keys():
@@ -27,11 +32,14 @@ func _process(delta: float) -> void:
 	pass
 
 func add_visitor() -> MarginContainer:
+	order_popout.show() # by lumiere
+	popout_timer.start(3.0)
+	
 	var newvisitor = visitor.duplicate()
 	newvisitor.visible = true
 	vbox.add_child(newvisitor)
 	return newvisitor
-	
+
 func add_order() -> void:
 	var newvisitor = add_visitor()
 	newvisitor.get_node("TileMapLayer").set_cell(Vector2i(0,0),0,Vector2i(randi_range(0,5),0))
@@ -46,3 +54,6 @@ func remove_visitor(orderno : int) -> void:
 		allvisitors[0].queue_free()
 		allvisitors.pop_front()
 	
+
+func _on_popout_timer_timeout() -> void:
+	order_popout.hide()
